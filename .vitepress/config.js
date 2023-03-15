034f81dd59ab3a94f10a27682aa37dea42b3e839
yangbingrui <yangbingrui@grgbanking.com>
2023-03-15 10:31:50 +0800

    dev

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  outDir: "../docs",
   base: '/notes/',
   title: "YBR notes",
   description: "record life and word.",
