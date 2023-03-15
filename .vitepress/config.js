e87ad2769f1927b4b6c7ecd03060458142ea8259
yangbingrui <yangbingrui@grgbanking.com>
2023-03-15 09:23:58 +0800

    dev

---
+++ .vitepress/config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/',
+  title: "YBR notes",
+  description: "record life and word.",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: "工作记录",
+        items: [
+          {
+            text: "2023",
+            items: [
+              {
+                text: "march",
+                items: [
+                  {
+                    text: "第二周",link: "/docs/work/2023/march/second"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/work/2023/march/second.md
@@ -0,0 +1,37 @@
+# tuesday
+
+今天在做需求时，要调整 antv@1.7.8 的消息提示模块的样式，由于这个大模块里面的消息提示比较多，这个样式的调整不能影响到其他的消息提示，结合文档 https://1x.antdv.com/components/message-cn/
+,想到一个实现方式，具体如下
+
+
+通过h方法返回一个带有标记的标签，包括要展示的文本信息
+```js
+this.$message.error({
+    content(h) {
+        // 标记子元素
+        return h("span", {attrs: {id: "uniqueId"}, "msg"})
+    }
+})
+```
+通过`:has()`伪类，用子元素来找出父元素，再改其样式
+```less
+.ant-message-notice-content {
+  &:has(#uniqueId) {
+    // 样式代码
+  }
+}
+```
+如此就不会影响到其他逻辑分支的消息弹窗了
+
+
+
+live demo
+::: raw
+
+<iframe src="https://codesandbox.io/embed/vue-antd-template-forked-gu3l3u?fontsize=14&hidenavigation=1&theme=dark"
+     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     title="Vue Antd Template (forked)"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+   ></iframe>
+:::
